ae4df0a0dd3772d821697d2f6bca5577984acc92
akshit0421 <akshit0421@gmail.com>
2025-10-16 11:13:08 +0530

    lab work

---
+++ Data Collection Book Labs/Advanced Data Display/2_Compositional-Layout_Lab/EmojiDictionary/Emoji.swift
@@ -0,0 +1,26 @@
+import Foundation
+import UIKit
+
+struct Emoji: Codable, Identifiable, Equatable {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    var id: String {
+        symbol
+    }
+    
+    var sectionTitle: String {
+        String(name.uppercased().first ?? "?")
+    }
+    
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+enum Layout {
+    case grid
+    case column
+}
